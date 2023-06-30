988405fbf4af6a94f9309fceac208cc51bd592bf
dUPYeYE <robert.boros12@gmail.com>
2023-06-30 23:47:43 +0300

    Made some progress.

--- app/index.js
+++ app/index.js
@@ -6,10 +6,41 @@ import { COLORS, icons, images, SIZES } from '../constants'
 import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components'
 
 const Home = () => {
+    const router = useRouter();
+
     return (
-        <View>
-            
-        </View>
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+            <Stack.Screen 
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerLeft: () => {
+                        return (
+                            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+                        )
+                    },
+                    headerRight: () => {
+                        return (
+                            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+                        )
+                    },
+                    headerTitle: ""
+                }}
+            />
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View
+                    style={{
+                        flex: 1,
+                        padding: SIZES.medium
+                    }}
+                >
+                    <Welcome />
+                    <Popularjobs />
+                    <Nearbyjobs />
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
